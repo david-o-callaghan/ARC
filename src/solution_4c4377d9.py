650c6e602ce8d3b52697b7e9863cf9e74b1b2d64
ajinkyasakhare19 <ajinkyasakhare19@gmail.com>
2019-11-19 01:52:32 +0530

    Added comments and solve function description

--- src/solution_4c4377d9.py
+++ src/solution_4c4377d9.py
@@ -8,10 +8,18 @@ import solver_utils
 import numpy as np
 
 def solve(inputmatrix):
-    y = np.array(inputmatrix)
-    y_copy=y
-    y=y[[2,1,0], :]
-    return((np.concatenate((y, y_copy))).tolist())
+    """
+            This function contains a solution to the data in 4be741c5.json posed by the Abstraction and
+            Reasoning Corpus (ARC).
+
+            The problem presents an n x m grid, with some rows containing 0-m coloured squares with repetition over a row or colomuns.
+            The solution requires the same colour pattern to be first mirrored and then concatenated with the older pattern. which
+            in result will create a grid of n x2m.
+        """
+    y = np.array(inputmatrix)#convert input matrix to numpy array
+    y_copy=y#Make a copy of existing pattern
+    y=y[[2,1,0], :]#swap first and last rows with each other
+    return((np.concatenate((y, y_copy))).tolist())#return a concatinated mirrored matrix to the old stored in y_copy
 
 # Use main() from solution_4be741c5.py as template
 if __name__ == "__main__":
