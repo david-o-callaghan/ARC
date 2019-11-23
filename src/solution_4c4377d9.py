c218017ae5fe8cb81294a28a613fb557927828b4
David OCallaghan <d.ocallaghan9@nuigalway.ie>
2019-11-23 17:56:41 +0000

    Made use of np.flip() for a more generalised solution and improved readability.

--- src/solution_4c4377d9.py
+++ src/solution_4c4377d9.py
@@ -9,24 +9,27 @@ import numpy as np
 
 def solve(inputmatrix):
     """
-            This function contains a solution to the data in 4be741c5.json posed by the Abstraction and
-            Reasoning Corpus (ARC).
+    This function contains a solution to the data in 4be741c5.json posed by the 
+    Abstraction and Reasoning Corpus (ARC).
 
-            The problem presents an n x m grid, with some rows containing 0-m coloured squares with repetition over a row or colomuns.
-            The solution requires the same colour pattern to be first mirrored and then concatenated with the older pattern. which
-            in result will create a grid of n x2m.
-        """
-    y = np.array(inputmatrix)#convert input matrix to numpy array
-    y_copy=y#Make a copy of existing pattern
-    y=y[[2,1,0], :]#swap first and last rows with each other
-    return((np.concatenate((y, y_copy))).tolist())#return a concatinated mirrored matrix to the old stored in y_copy
+    The problem presents an n-by-m grid. The solution requires the same colour 
+    pattern as the input to be mirrored and then concatenated with the original
+    pattern. This results in an output grid of n-by-2m.
+    """
+    
+    y = np.array(inputmatrix) # convert input matrix to numpy array
+    y_mirrored = np.flip(y, axis=0) # swap first and last rows with each other
+    
+    # return a concatinated mirrored matrix to the old stored in y_copy
+    return np.concatenate((y_mirrored, y)).tolist() 
 
-# Use main() from solution_4be741c5.py as template
-if __name__ == "__main__":
+if __name__=='__main__':
+    # Get the data for the associated JSON file
     data = solver_utils.parse_json_file()
-
-    for training in data['train']:
-        solver_utils.solve_wrapper(training['input'], solve)
-
-    for testing in data['test']:
-        solver_utils.solve_wrapper(testing['input'], solve)
+    
+    # Iterate through training grids and test grids
+    for data_train in data['train']:
+        solver_utils.solve_wrapper(data_train['input'], solve)
+        
+    for data_test in data['test']:
+        solver_utils.solve_wrapper(data_test['input'], solve)
