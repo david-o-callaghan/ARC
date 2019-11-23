e3832021d2758076931d2e9dc6de217350100a4e
David OCallaghan <d.ocallaghan9@nuigalway.ie>
2019-11-23 18:05:59 +0000

    Added basic docstrings to functions

--- src/solver_utils.py
+++ src/solver_utils.py
@@ -2,6 +2,11 @@ import argparse
 import json
 
 def print_grid(grid_in):
+    """
+    Prints the elements of grid_in with a space between each element along the
+    columns and a new line for each row. Assumes that grid_in is a list of
+    lists.
+    """
     #iterate through grid
     for row in grid_in:
         for elem in row:
@@ -10,6 +15,9 @@ def print_grid(grid_in):
         print() # go to new line
 
 def solve_wrapper(data_in, solver):
+    """
+    Calls the solver function on data_in and calls print_grid on the result.
+    """
     # Call the solver function
     data_out = solver(data_in)    
     
@@ -18,6 +26,10 @@ def solve_wrapper(data_in, solver):
     print()
     
 def get_colour_code(colour):
+    """
+    This function returns the integer associated with the input string for the
+    ARC problems.
+    """
     colour_mapping = {'black':0,
                       'blue':1,
                       'red':2,
@@ -32,6 +44,10 @@ def get_colour_code(colour):
     return colour_mapping[colour]
  
 def parse_json_file():
+    """
+    This function reads the data from the JSON file passed from the command
+    line and returns the data after converting it to a Python dictionary.
+    """
     # Get file name passed from command line
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file_json")
